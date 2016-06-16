2c1483d328b528311b2d41ea18b3954cf4d495a6
Divan Visagie <visagiedivan@gmail.com>
2016-06-16 11:17:43 +0200

    update svc to use with api

--- client/src/main/scala/com/client/ThriftClient.scala
+++ client/src/main/scala/com/client/ThriftClient.scala
@@ -2,29 +2,25 @@ package com.client
 
 import com.fib.thrift.math.thriftscala.MathService
 import com.twitter.finagle._
-import com.twitter.finagle.param.{Tracer => PTracer}
 import com.twitter.finagle.stats.NullStatsReceiver
 import com.twitter.finagle.tracing._
 import com.twitter.util.{Await, Future}
 
 import scala.language.reflectiveCalls
 
-
 object ThriftClient {
 
   val svc: MathService[Future] = ThriftMux.client
-      .configured(param.Tracer(NullTracer))
-      .configured(param.Stats(NullStatsReceiver))
-      .newIface[MathService.FutureIface]("localhost:9090")
+    .withTracer(NullTracer)
+    .withStatsReceiver(NullStatsReceiver)
+    .newIface[MathService.FutureIface]("localhost:9090")
 
   def main(args: Array[String]) {
 
-    Await.ready(svc.fibonacci(30)).onSuccess( f => {
+    Await.ready(svc fibonacci 30).onSuccess( f =>
       println(s"fib 30: ${f.toString}")
-    }).onFailure( err => {
+    ).onFailure( err =>
       println(s"error: ${err.getMessage}")
-    })
+    )
   }
 }
-
-
